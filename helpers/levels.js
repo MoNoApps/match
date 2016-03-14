41a2f5a0664128f380fb991fdcb7d83b72beb015
Daniel Martinez <dmartz86@gmail.com>
2016-03-13 22:09:59 -0500

    Removed semicolon

--- helpers/levels.js
+++ helpers/levels.js
@@ -1,7 +1,7 @@
 function shuffle(o) {
   for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
   return o;
-};
+}
 
 function level (first, last) {
   var range = [];
